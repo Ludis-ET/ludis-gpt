c278935b0f7db82609e9279017a99fd9b8592211
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-02 18:35:46 +0300

    Update chat messages

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,155 +1,171 @@
+import logo from "../assets/logo.png";
 export const Chat = () => {
   return (
     <div className="w-full px-5 flex flex-col justify-between">
       <div className="flex flex-col mt-5">
         <div className="flex justify-end mb-4">
           <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-            Welcome to group everyone !
+            Help me Ludis
           </div>
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={"https://source.unsplash.com/vpOeXr5wmR4/600x600"}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
         </div>
         <div className="flex justify-start mb-4">
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={logo}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
           <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat at
-            praesentium, aut ullam delectus odio error sit rem. Architecto nulla
-            doloribus laborum illo rem enim dolor odio saepe, consequatur quas?
-          </div>
-        </div>
-        <div className="flex justify-end mb-4">
-          <div>
-            <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam,
-              repudiandae.
-            </div>
-
-            <div className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-              reiciendis!
-            </div>
-          </div>
-          <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-            className="object-cover h-8 w-8 rounded-full"
-            alt=""
-          />
-        </div>
-        <div className="flex justify-start mb-4">
-          <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-            className="object-cover h-8 w-8 rounded-full"
-            alt=""
-          />
-          <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-            happy holiday guys!
+            {` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+            accusantium fugiat perferendis ut expedita aliquid minus, accusamus
+            enim veritatis deleniti aperiam ratione rerum deserunt distinctio
+            eveniet! Illum ut asperiores atque! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem, doloremque rerum deserunt tempora
+            aspernatur molestias eos itaque accusantium est natus iure omnis ea
+            quae dolor? Reiciendis dolorem nostrum voluptatem ex. \n\n Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Officia, voluptatibus
+            quam cum nisi aspernatur qui maxime a vitae quis eaque,
+            reprehenderit, itaque nesciunt doloremque possimus asperiores ab
+            beatae recusandae. Illo? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Libero nisi maxime quod facere, numquam ex magni
+            eaque? Error enim blanditiis ea placeat, voluptatibus odio libero?
+            Nisi commodi perferendis nesciunt deleniti.`}
           </div>
         </div>
         <div className="flex justify-end mb-4">
           <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-            Welcome to group everyone !
+            Help me Ludis
           </div>
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={"https://source.unsplash.com/vpOeXr5wmR4/600x600"}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
         </div>
         <div className="flex justify-start mb-4">
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={logo}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
           <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat at
-            praesentium, aut ullam delectus odio error sit rem. Architecto nulla
-            doloribus laborum illo rem enim dolor odio saepe, consequatur quas?
-          </div>
-        </div>
-        <div className="flex justify-end mb-4">
-          <div>
-            <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam,
-              repudiandae.
-            </div>
-
-            <div className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-              reiciendis!
-            </div>
-          </div>
-          <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-            className="object-cover h-8 w-8 rounded-full"
-            alt=""
-          />
-        </div>
-        <div className="flex justify-start mb-4">
-          <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-            className="object-cover h-8 w-8 rounded-full"
-            alt=""
-          />
-          <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-            happy holiday guys!
+            {` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+            accusantium fugiat perferendis ut expedita aliquid minus, accusamus
+            enim veritatis deleniti aperiam ratione rerum deserunt distinctio
+            eveniet! Illum ut asperiores atque! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem, doloremque rerum deserunt tempora
+            aspernatur molestias eos itaque accusantium est natus iure omnis ea
+            quae dolor? Reiciendis dolorem nostrum voluptatem ex. \n\n Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Officia, voluptatibus
+            quam cum nisi aspernatur qui maxime a vitae quis eaque,
+            reprehenderit, itaque nesciunt doloremque possimus asperiores ab
+            beatae recusandae. Illo? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Libero nisi maxime quod facere, numquam ex magni
+            eaque? Error enim blanditiis ea placeat, voluptatibus odio libero?
+            Nisi commodi perferendis nesciunt deleniti.`}
           </div>
         </div>
         <div className="flex justify-end mb-4">
           <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-            Welcome to group everyone !
+            Help me Ludis
           </div>
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={"https://source.unsplash.com/vpOeXr5wmR4/600x600"}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
         </div>
         <div className="flex justify-start mb-4">
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={logo}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
           <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat at
-            praesentium, aut ullam delectus odio error sit rem. Architecto nulla
-            doloribus laborum illo rem enim dolor odio saepe, consequatur quas?
+            {` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+            accusantium fugiat perferendis ut expedita aliquid minus, accusamus
+            enim veritatis deleniti aperiam ratione rerum deserunt distinctio
+            eveniet! Illum ut asperiores atque! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem, doloremque rerum deserunt tempora
+            aspernatur molestias eos itaque accusantium est natus iure omnis ea
+            quae dolor? Reiciendis dolorem nostrum voluptatem ex. \n\n Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Officia, voluptatibus
+            quam cum nisi aspernatur qui maxime a vitae quis eaque,
+            reprehenderit, itaque nesciunt doloremque possimus asperiores ab
+            beatae recusandae. Illo? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Libero nisi maxime quod facere, numquam ex magni
+            eaque? Error enim blanditiis ea placeat, voluptatibus odio libero?
+            Nisi commodi perferendis nesciunt deleniti.`}
           </div>
         </div>
         <div className="flex justify-end mb-4">
-          <div>
-            <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam,
-              repudiandae.
-            </div>
-
-            <div className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-              reiciendis!
-            </div>
+          <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+            Help me Ludis
           </div>
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={"https://source.unsplash.com/vpOeXr5wmR4/600x600"}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
         </div>
         <div className="flex justify-start mb-4">
           <img
-            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+            src={logo}
             className="object-cover h-8 w-8 rounded-full"
             alt=""
           />
           <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-            happy holiday guys!
+            {` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+            accusantium fugiat perferendis ut expedita aliquid minus, accusamus
+            enim veritatis deleniti aperiam ratione rerum deserunt distinctio
+            eveniet! Illum ut asperiores atque! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem, doloremque rerum deserunt tempora
+            aspernatur molestias eos itaque accusantium est natus iure omnis ea
+            quae dolor? Reiciendis dolorem nostrum voluptatem ex. \n\n Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Officia, voluptatibus
+            quam cum nisi aspernatur qui maxime a vitae quis eaque,
+            reprehenderit, itaque nesciunt doloremque possimus asperiores ab
+            beatae recusandae. Illo? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Libero nisi maxime quod facere, numquam ex magni
+            eaque? Error enim blanditiis ea placeat, voluptatibus odio libero?
+            Nisi commodi perferendis nesciunt deleniti.`}
+          </div>
+        </div>
+        <div className="flex justify-end mb-4">
+          <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+            Help me Ludis
+          </div>
+          <img
+            src={"https://source.unsplash.com/vpOeXr5wmR4/600x600"}
+            className="object-cover h-8 w-8 rounded-full"
+            alt=""
+          />
+        </div>
+        <div className="flex justify-start mb-4">
+          <img
+            src={logo}
+            className="object-cover h-8 w-8 rounded-full"
+            alt=""
+          />
+          <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+            {` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+            accusantium fugiat perferendis ut expedita aliquid minus, accusamus
+            enim veritatis deleniti aperiam ratione rerum deserunt distinctio
+            eveniet! Illum ut asperiores atque! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem, doloremque rerum deserunt tempora
+            aspernatur molestias eos itaque accusantium est natus iure omnis ea
+            quae dolor? Reiciendis dolorem nostrum voluptatem ex. \n\n Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Officia, voluptatibus
+            quam cum nisi aspernatur qui maxime a vitae quis eaque,
+            reprehenderit, itaque nesciunt doloremque possimus asperiores ab
+            beatae recusandae. Illo? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Libero nisi maxime quod facere, numquam ex magni
+            eaque? Error enim blanditiis ea placeat, voluptatibus odio libero?
+            Nisi commodi perferendis nesciunt deleniti.`}
           </div>
         </div>
       </div>
